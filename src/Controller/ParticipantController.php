2c6627c9b4447ced3a583391bf7fb301d49bd2c8
Catherine Polikarpova <espolikarpova@yandex.ru>
2024-10-29 23:48:43 +0300

    part

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant", name="participant_index")
+     */
+    public function index(ParticipantRepository $participantRepository): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'participants' => $participantRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/participant/new", name="participant_new", methods={"GET","POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('participant_index');
+        }
+
+        return $this->render('participant/new.html.twig', [
+            'participant' => $participant,
+            'form' => $form->createView(),
+        ]);
+    }
+}
